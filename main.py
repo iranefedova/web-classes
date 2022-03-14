6087b1e09aa3daba4e45bc8697d34c703d1aa174
Ira Nefedova <iraonegina@yandex.ru>
2022-03-14 18:19:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+from data.news import News
+from flask import render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+if __name__ == '__main__':
+    main()
